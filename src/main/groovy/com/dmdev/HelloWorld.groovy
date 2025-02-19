bda9b701da96982450c419da4da053160ba1ee34
Lenar Sharipov <lenarsharipov@gmail.com>
2025-02-19 10:52:11 +0300

    lesson-04

--- src/main/groovy/com/dmdev/HelloWorld.groovy
+++ src/main/groovy/com/dmdev/HelloWorld.groovy
@@ -2,12 +2,20 @@ package com.dmdev
 
 import groovy.transform.PackageScope
 
+import java.sql.DriverManager
+
 class HelloWorld {
 
     Integer value
 
 //    @PackageScope
     static void main(String[] args) {
+        String value = "Hello World"
+        Class<String> clazz = value.getClass()
+        assert clazz == String.class
         println "Hello World!"
+        println clazz.getClassLoader() // null
+        println DriverManager.class.getClassLoader() // jdk.internal.loader.ClassLoaders$PlatformClassLoader@3a93b025
+        println HelloWorld.class.getClassLoader() // jdk.internal.loader.ClassLoaders$AppClassLoader@639fee48
     }
 }
