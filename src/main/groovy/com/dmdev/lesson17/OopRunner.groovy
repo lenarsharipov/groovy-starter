025095e61220e86d211dffdffa1ec73670dc9ebd
Lenar Sharipov <lenarsharipov@gmail.com>
2025-03-03 11:01:42 +0300

    lesson17 added

---
+++ src/main/groovy/com/dmdev/lesson17/OopRunner.groovy
@@ -0,0 +1,27 @@
+package com.dmdev.lesson17
+
+class OopRunner {
+
+    static void main(String[] args) {
+        def student = new Student()
+        student.firstName = "Ivan" // setFirstName
+        println student.firstName // getFirstName
+        println student['firstName']
+        println student.'firstName'
+        println student.@firstName  // обращение к полу напрямую. Стоит избегать такое
+
+        // не создавая кастомных конструкторов, мы можем инициализировать все поля
+        def student1 = new Student(firstName: "Petr", lastName: "Petrov", age: 18)
+        println student1 // com.dmdev.lesson17.Student(Petr, Petrov, 18)
+
+        // принуждение - , через массив аргументов
+        Student student2 = ['Sveta', 'Svetikova', 30]
+        println student2 // com.dmdev.lesson17.Student(Sveta, Svetikova, 30)
+
+        def (firstName, lastName) = student2 // тут используется метод getAt(index) in Student class
+        println firstName // Sveta
+        println lastName // Svetikova
+
+        assert [student1, student2].collect {it.firstName } == ['Petr', 'Sveta']
+    }
+}
