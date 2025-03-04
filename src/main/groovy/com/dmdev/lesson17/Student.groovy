12b2544d97e53a009f2d747d168a2bbc235fce63
Lenar Sharipov <lenarsharipov@gmail.com>
2025-03-04 10:59:04 +0300

    lesson20 added

--- src/main/groovy/com/dmdev/lesson17/Student.groovy
+++ src/main/groovy/com/dmdev/lesson17/Student.groovy
@@ -1,10 +1,21 @@
 package com.dmdev.lesson17
 
+import groovy.transform.Canonical
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.Immutable
 import groovy.transform.ToString
 import groovy.transform.TupleConstructor
+import groovy.transform.builder.Builder
+import groovy.util.logging.Slf4j
 
 @ToString
-@TupleConstructor // создает конструкторы со всеми комбинациями
+@TupleConstructor
+// создает конструкторы со всеми комбинациями
+@EqualsAndHashCode
+//@Canonical
+@Immutable
+@Builder
+//@Slf4j
 class Student implements WithId {
     String firstName
     String lastName
@@ -12,5 +23,12 @@ class Student implements WithId {
 
     def getAt(Integer index) {
         index == 0 ? firstName : lastName
+    }
+
+    static void main(String[] args) {
+        def student = builder()
+                .lastName("Ivanov")
+                .firstName("Ivan")
+                .build()
     }
 }
